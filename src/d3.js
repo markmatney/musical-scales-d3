9034a8090a5e30edc54071ec3d1d241511c7d8a8
Mark Matney <mmatney@library.ucla.edu>
2016-10-22 21:18:34 -0700

    simplify d3 module

--- src/d3.js
+++ src/d3.js
@@ -1,9 +1,5 @@
-var selection = require('d3-selection'),
-    scale = require('d3-scale'),
-    transition = require('d3-transition');
-
 module.exports = Object.assign({},
-    selection,
-    scale,
-    transition
+    require('d3-scale'),
+    require('d3-selection'),
+    require('d3-transition')
 );
